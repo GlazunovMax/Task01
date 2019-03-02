65f9b976f5c165abfda71072439bd76f01846a80
Asus <Asus@DESKTOP-6FNF5IC>
2019-03-02 22:25:19 +0300

    add test entity

--- Task01/test/by/epam/javatraining/glazunov/task01/model/entity/LocomotiveTest.java
+++ Task01/test/by/epam/javatraining/glazunov/task01/model/entity/LocomotiveTest.java
@@ -1,22 +1,42 @@
 package by.epam.javatraining.glazunov.task01.model.entity;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import by.epam.javatraining.glazunov.task01.model.exception.MarkLocomotiveIsEmptyException;
+
 public class LocomotiveTest {
-	
-	@Test
-	public void setMarkShouldBeEmpty() {
+	private static Locomotive locomotive;
+
+	@BeforeClass
+	public static void createLocomotive() {
+		locomotive = new Locomotive();
+	}
+
+	@Test(expected = MarkLocomotiveIsEmptyException.class)
+	public void setMarkShouldBeEmpty() throws MarkLocomotiveIsEmptyException {
 		String mark = "";
-		
-		Locomotive locomotive = new Locomotive();
-		if(mark.isEmpty()) {
-			mark = "Not assigned";
-		}
+
+		locomotive.setMark(mark);
+	}
+
+	@Test
+	public void setMarkNotEmpty() throws MarkLocomotiveIsEmptyException {
+		String mark = "Mark";
+
 		locomotive.setMark(mark);
 
-		Assert.assertEquals("Not assigned", locomotive.getMark());
+		String actual = locomotive.getMark();
+		String expected = "Mark";
+
+		Assert.assertEquals(expected, actual);
 	}
-	
+
+	@AfterClass
+	public static void deleteTrain() {
+		locomotive = null;
+	}
 
 }
--- Task01/test/by/epam/javatraining/glazunov/task01/model/entity/LuggageWaggonTest.java
+++ Task01/test/by/epam/javatraining/glazunov/task01/model/entity/LuggageWaggonTest.java
@@ -1,25 +1,41 @@
 package by.epam.javatraining.glazunov.task01.model.entity;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import by.epam.javatraining.glazunov.task01.model.exception.LuggageWeightNegativeException;
+
 public class LuggageWaggonTest {
-
-	@Test
-	public void testSetLuggage() {
-		double luggage = -5.0;
-
-		LuggageWaggon luggageWaggon = new LuggageWaggon();
-		if (luggage < 0) {
-			luggage = 0.0;
-		}
-
-		luggageWaggon.setLuggage(luggage);
-
-		Object expected = 0.0;
-
-		Assert.assertEquals(expected, luggageWaggon.getLuggage());
-
+	private static LuggageWaggon luggageWaggon;
+	
+	@BeforeClass
+	public static void createLuggageWaggon() {
+		luggageWaggon = new LuggageWaggon();
 	}
 
+	@Test(expected = LuggageWeightNegativeException.class)
+	public void testSetLuggageNegative() throws LuggageWeightNegativeException {
+		double luggage = -5.0;
+
+		luggageWaggon.setLuggageWeight(luggage);
+	}
+	
+	@Test()
+	public void testSetLuggage() throws LuggageWeightNegativeException  {
+		double luggage = 5.0;
+
+		luggageWaggon.setLuggageWeight(luggage);
+
+		double expected = 5.0;
+		double actual = luggageWaggon.getLuggageWeight();
+
+		Assert.assertEquals(expected, actual, 0.001);
+	}
+
+	@AfterClass
+	public static void deleteLuggageWaggon() {
+		luggageWaggon = null;
+	}
 }
