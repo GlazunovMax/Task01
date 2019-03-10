2475dbbf52f92f866d30b6311405c2f23ae5d722
Asus <Asus@DESKTOP-6FNF5IC>
2019-03-10 21:21:18 +0300

    change buildTrain class

--- Task01/src/by/epam/javatraining/glazunov/task01/utill/BuildTrain.java
+++ Task01/src/by/epam/javatraining/glazunov/task01/utill/BuildTrain.java
@@ -1,8 +1,6 @@
 package by.epam.javatraining.glazunov.task01.utill;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import by.epam.javatraining.glazunov.task01.model.entity.Locomotive;
@@ -15,18 +13,10 @@ import by.epam.javatraining.glazunov.task01.model.entity.Waggon;
 import by.epam.javatraining.glazunov.task01.model.exception.MarkLocomotiveIsEmptyException;
 
 public class BuildTrain {
-	//private static final String MESSAGE_MARK_LOCO_IS_EMPTY = "Mark not specified!";
 	static Random random = new Random();
 
-	public Train createTrain(String nameTrain, String mark) {
-		Train train = null;
-
-		train = new Train(nameTrain, createLocomotie(mark), createWaggons());
-
-		train.setOccupiedPlaces(fillPassengerSeats(train));
-
-		train.setOccupiedLuggage(fillLuggagePassenger(train));
-
+	public static Train createTrain(String nameTrain, String mark) {
+		Train train = new Train(nameTrain, createLocomotie(mark), createWaggons());
 		return train;
 	}
 
@@ -39,7 +29,7 @@ public class BuildTrain {
 		} catch (MarkLocomotiveIsEmptyException e) {
 			System.err.println(e.getMessage());
 		}
-		
+
 		locomotive.setTypeLocomotive(type[random.nextInt(type.length)]);
 
 		return locomotive;
@@ -47,56 +37,37 @@ public class BuildTrain {
 	}
 
 	private static final int MIN_COUNT_PASSENGER_WAGGONS = 2;
-	private static final int MAX_COUNT_PASSENGER_WAGGONS = 18;
+	private static final int MAX_COUNT_PASSENGER_WAGGONS = 8;
 	private static final int MAX_COUNT_LUGGAGE_WAGGONS = 2;
-	private static final double LEHGHT_PASSENGER_WAGGONS = 24.5;
-	private static final double LEHGHT_LUGGAGE_WAGGONS = 29.3;
-	private static final double MAX_LUGGAGE_IN_LAGGAGE_WAGGONS = 3500;
+	private static final double MAX_LUGGAGE_IN_LAGGAGE_WAGGON = 3500;
 
-	private static List<Waggon> createWaggons() {
-		List<Waggon> waggons = new ArrayList<>();
+	private static Waggon[] createWaggons() {
+		int countPassengerWaggon = random.nextInt(MAX_COUNT_PASSENGER_WAGGONS) + MIN_COUNT_PASSENGER_WAGGONS;
+		int countLuggageWaggon = random.nextInt(MAX_COUNT_LUGGAGE_WAGGONS);
+		int allWaggons = countLuggageWaggon + countPassengerWaggon;
 
-		for (int i = 0; i < random.nextInt(MAX_COUNT_PASSENGER_WAGGONS) + MIN_COUNT_PASSENGER_WAGGONS; i++) {
-			waggons.add(new PassengerWaggon(LEHGHT_PASSENGER_WAGGONS,
-					TypePassengerWaggon.values()[random.nextInt(TypePassengerWaggon.values().length)]));
+		Waggon[] waggons = new Waggon[allWaggons];
+
+		TypePassengerWaggon typeWaggon;
+		int occupiedPlaces;
+
+		for (int i = 0; i < countPassengerWaggon; i++) {
+			typeWaggon = TypePassengerWaggon.values()[random.nextInt(TypePassengerWaggon.values().length)];
+			occupiedPlaces = random.nextInt(typeWaggon.getNumberSeats());
+
+			waggons[i] = new PassengerWaggon(typeWaggon, occupiedPlaces);
 		}
 
-		for (int i = 0; i < random.nextInt(MAX_COUNT_LUGGAGE_WAGGONS); i++) {
-			waggons.add(new LuggageWaggon(LEHGHT_LUGGAGE_WAGGONS, MAX_LUGGAGE_IN_LAGGAGE_WAGGONS));
+		BigDecimal occupiedLuggage;
+		for (int i = countPassengerWaggon; i < waggons.length; i++) {
+			occupiedLuggage = new BigDecimal(random.nextDouble() * MAX_LUGGAGE_IN_LAGGAGE_WAGGON).setScale(3,
+					BigDecimal.ROUND_HALF_DOWN);
+
+			waggons[i] = new LuggageWaggon(MAX_LUGGAGE_IN_LAGGAGE_WAGGON, occupiedLuggage);
 		}
 
 		return waggons;
 
-	}
-
-	private static BigDecimal fillLuggagePassenger(Train train) {
-		List<Waggon> waggons = train.getWaggons();
-
-		BigDecimal rank = train.getTotalWeightLuggage();
-
-		BigDecimal occupiedLuggage = new BigDecimal(0);
-
-		for (Waggon waggon : waggons) {
-			if (waggon instanceof LuggageWaggon) {
-				occupiedLuggage = occupiedLuggage.add(new BigDecimal(random.nextDouble() * rank.doubleValue()));
-			}
-		}
-
-		return occupiedLuggage.setScale(3, BigDecimal.ROUND_HALF_DOWN);
-
-	}
-
-	private static int fillPassengerSeats(Train train) {
-		List<Waggon> waggons = train.getWaggons();
-		int countOccupiedPassengerSeats = 0;
-
-		for (Waggon waggon : waggons) {
-			if (waggon instanceof PassengerWaggon) {
-				countOccupiedPassengerSeats += random
-						.nextInt(((PassengerWaggon) waggon).getTypeWaggon().getNumberSeats());
-			}
-		}
-		return countOccupiedPassengerSeats;
 	}
 
 }
