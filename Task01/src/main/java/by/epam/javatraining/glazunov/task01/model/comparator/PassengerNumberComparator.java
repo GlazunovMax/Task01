9ead77c1b5009a5c04a69332515a35de40414285
Asus <Asus@DESKTOP-6FNF5IC>
2019-03-15 15:27:11 +0300

    change comparators

--- Task01/src/main/java/by/epam/javatraining/glazunov/task01/model/comparator/PassengerNumberComparator.java
+++ Task01/src/main/java/by/epam/javatraining/glazunov/task01/model/comparator/PassengerNumberComparator.java
@@ -7,14 +7,14 @@ public class PassengerNumberComparator implements Comparator<Train> {
 
 	@Override
 	public int compare(Train train1, Train train2) {
-		if (train1.getTotalNumberPassengerSeats() < train2.getTotalNumberPassengerSeats()) {
+		/*if (train1.getTotalNumberPassengerSeats() < train2.getTotalNumberPassengerSeats()) {
 			return 1;
 		} else if (train1.getTotalNumberPassengerSeats() > train2.getTotalNumberPassengerSeats()) {
 			return -1;
 		} else {
 			return 0;
-		}
+		}*/
 		
-		//return Integer.compare(train2.getTotalNumberPassengerSeats(), train1.getTotalNumberPassengerSeats());
+		return Integer.compare(train2.getTotalNumberPassengerSeats(), train1.getTotalNumberPassengerSeats());
 	}
 }
