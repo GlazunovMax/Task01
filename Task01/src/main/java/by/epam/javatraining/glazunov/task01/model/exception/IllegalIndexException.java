a19758d83f016a2ce81aa40d2134fd47157146f2
Glazunov <Asus@DESKTOP-6FNF5IC>
2019-03-20 15:54:34 +0300

    add new exception

---
+++ Task01/src/main/java/by/epam/javatraining/glazunov/task01/model/exception/IllegalIndexException.java
@@ -0,0 +1,13 @@
+package by.epam.javatraining.glazunov.task01.model.exception;
+
+public class IllegalIndexException extends LogicException {
+	
+	public IllegalIndexException() {
+		super();
+	}
+
+	public IllegalIndexException(String message) {
+		super(message);
+	}
+
+}
